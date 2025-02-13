5aff6025ff31880e19c093f20fd678b312ce6098
Drew Swinney <drewswinney@gmail.com>
2025-02-12 21:50:05 -0500

    Initial commit

---
+++ scenes/Player.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+}
