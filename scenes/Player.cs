6fe77dca23649aedfd6bbf474a7b5a1c0177bf82
Drew Swinney <drewswinney@gmail.com>
2025-02-12 23:41:22 -0500

    Add character and first room

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -3,11 +3,28 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public float Speed = 100.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		
+    public override void _Ready()
+    {
+        base._Ready();
+
+		this.Position = new Vector2(440, 160);
+    }
+
+    public override void _PhysicsProcess(double delta)
+	{if (Input.IsKeyPressed(Key.Right)) {
+			this.Velocity = new Vector2(this.Speed * (float)delta, 0);
+		} else if (Input.IsKeyPressed(Key.Left)) {
+			this.Velocity = new Vector2(-1 * this.Speed * (float)delta, 0);
+		} else if (Input.IsKeyPressed(Key.Up)) {
+			this.Velocity = new Vector2(0, -1 * this.Speed * (float)delta);
+		} else if (Input.IsKeyPressed(Key.Down)) {
+			this.Velocity = new Vector2(0, this.Speed * (float)delta);
+		} else {
+			this.Velocity = new Vector2(0, 0);
+		}
+
+		this.MoveAndCollide(this.Velocity);
 	}
 }
