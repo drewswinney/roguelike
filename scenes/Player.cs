b252f2ad1f43b22d4082654e71b50273890a6232
Drew Swinney <drewswinney@gmail.com>
2025-02-13 23:59:59 -0500

    Update character and fix tilemap

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -1,27 +1,39 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
-	public float Speed = 100.0f;
+	public float Speed = 320.0f;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
 		this.Position = new Vector2(440, 160);
-    }
+		var idle = this.GetNode<AnimatedSprite2D>("IdleAnimation");
+		idle.SpeedScale = 0.5f;
+		idle.Play();
+	}
 
-    public override void _PhysicsProcess(double delta)
-	{if (Input.IsKeyPressed(Key.Right)) {
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Input.IsKeyPressed(Key.Right))
+		{
 			this.Velocity = new Vector2(this.Speed * (float)delta, 0);
-		} else if (Input.IsKeyPressed(Key.Left)) {
+		}
+		else if (Input.IsKeyPressed(Key.Left))
+		{
 			this.Velocity = new Vector2(-1 * this.Speed * (float)delta, 0);
-		} else if (Input.IsKeyPressed(Key.Up)) {
+		}
+		else if (Input.IsKeyPressed(Key.Up))
+		{
 			this.Velocity = new Vector2(0, -1 * this.Speed * (float)delta);
-		} else if (Input.IsKeyPressed(Key.Down)) {
+		}
+		else if (Input.IsKeyPressed(Key.Down))
+		{
 			this.Velocity = new Vector2(0, this.Speed * (float)delta);
-		} else {
+		}
+		else
+		{
 			this.Velocity = new Vector2(0, 0);
 		}
 
